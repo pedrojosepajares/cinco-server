03a0201724371ce7a3544457eccde94e9213202b
Pedro José Pajares Ramírez <polaris_3748@hotmail.com>
2017-06-07 19:02:24 +0200

    Conexión con la base de datos

--- cinco-server.js
+++ cinco-server.js
@@ -1,14 +1,23 @@
 var port = 3000;
+var dbUrl = "mongodb://localhost/",
+    dbName = "decks";
 
 var express = require('express'),
     app = express(),
     router = express.Router();
+    mongoose = require('mongoose');
 
 router.get('/', function(req, res){
     res.send("Hello World!");
 });
 
 app.use(router);
+
+// Conexión con la base de datos
+mongoose.connect(dbUrl+dbName, function(err, res){
+    if (err) console.log("ERROR: can't connect to database (" + err + ")");
+    else console.log("Connected to database " + dbName );
+});
 
 app.listen(port, function(){
     console.log("Server running on localhost:" + port);
