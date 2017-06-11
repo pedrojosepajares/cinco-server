ce41c315eab29cccc339787a4224ce0c7734792f
Pedro José Pajares Ramírez <polaris_3748@hotmail.com>
2017-06-11 20:15:55 +0200

    Solucionado problema 'no access-control-allow-origin header'

--- api/controllers/cinco-server-controller.js
+++ api/controllers/cinco-server-controller.js
@@ -3,6 +3,10 @@ var deckModel = require('../models/cinco-server-model');
 
 // GET - Devuelve todas las barajas en la BD
 exports.findAll = function(req, res){
+
+  // Cabecera para recibir peticiones desde todos los orígenes
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
   deckModel.find(function(err, decks){
     if (err) res.send(500, err.mesaage);
     console.log('GET /decks');
@@ -12,6 +16,10 @@ exports.findAll = function(req, res){
 
 // GET - Devuelve una baraja especificando con ID
 exports.find = function(req, res){
+
+  // Cabecera para recibir peticiones desde todos los orígenes
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
   deckModel.findById(req.params.id, function(err, deck){
     if(err) return res.send(500, err.message);
     console.log('GET /deck/' + req.params.id);
@@ -21,6 +29,10 @@ exports.find = function(req, res){
 
 // POST - Inserta una baraja en la BD
 exports.add = function(req, res){
+
+  // Cabecera para recibir peticiones desde todos los orígenes
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
   console.log('POST');
   console.log(req.body);
 
@@ -37,6 +49,10 @@ exports.add = function(req, res){
 
 // PUT- Actualiza una baraja existente
 exports.update = function(req, res){
+
+  // Cabecera para recibir peticiones desde todos los orígenes
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
   console.log('PUT /deck/' + req.params.id);
   deckModel.findById(req.params.id, function(err, deck){
     deck.name = req.body.name;
--- cinco-server.js
+++ cinco-server.js
@@ -22,7 +22,25 @@ mongoose.connect(dbUrl+dbName, function(err, res){
 var routes = require('./api/routes/cinco-server-routes');
 app.use(routes);
 
+// Cabeceras
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.listen(port, function(){
     console.log("Server running on localhost:" + port);
